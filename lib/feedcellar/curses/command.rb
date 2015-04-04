4257c9fa23bb111bcd31cdb10a4f61a703b32bf6
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-04-04 20:03:24 +0900

    Fix default directory

--- lib/feedcellar/curses/command.rb
+++ lib/feedcellar/curses/command.rb
@@ -29,7 +29,7 @@ module Feedcellar
 
       def initialize(*args)
         super
-        default_base_dir = File.join(File.expand_path("~"), ".feedcellar-curses")
+        default_base_dir = File.join(File.expand_path("~"), ".feedcellar")
         @base_dir = ENV["FEEDCELLAR_HOME"] || default_base_dir
         @database_dir = File.join(@base_dir, "db")
       end
